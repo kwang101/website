e0505cb9153961b7683d20fd9e552b7410288576
Kathy Wang <kathywang98@hotmail.com>
2019-02-08 22:55:48 -0800

    revamp 2

--- app/hello.py
+++ app/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, make_response
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -18,25 +18,10 @@ def projects():
 def posts():
 	return render_template('posts.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/docs/resume')
-def get_pdf(id=None):
-    if id is not None:
-        binary_pdf = get_binary_pdf_data_from_database(id=id)
-        response = make_response(binary_pdf)
-        response.headers['Content-Type'] = 'application/pdf'
-        response.headers['Content-Disposition'] = \
-            'inline; filename=%s.pdf' % 'Resume'
-        return response
-
-@app.route('/resume')
-def show_pdf(id=None):
-    if id is not None:
-        return render_template('doc.html', doc_id=id)
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(debug=True)
